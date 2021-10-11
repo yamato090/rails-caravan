3fc0083ca4179df18b503a31885f46955bb186f9
yamato090 <yamauchi09012@gmail.com>
2021-10-11 11:38:11 +0000

    add 7章【削除機能を作ろう】

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,6 @@
 class BlogsController < ApplicationController
   def index
-
+    @blogs = Blog.all
   end
 
   def show
@@ -14,8 +14,9 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path(blog.id)
-  end  
+    # 以下の行を修正
+    redirect_to blog_path(blog.id)
+  end
 
   def edit
     @blog = Blog.find(params[:id])
@@ -25,6 +26,13 @@ class BlogsController < ApplicationController
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
+  end  
+  
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+    
   end  
   
   private
